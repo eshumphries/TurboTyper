996076cc78775cb0afaf705dd4e628476fe488a9
eshumphries <eshumphries@outlook.com>
2025-06-16 20:37:47 -0400

    Placed all elements in a ScrollViewer instead of a Viewbox and added zooming feature to adjust everything according to web page size

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 
 namespace TurboTyper
 {
@@ -12,6 +13,17 @@ namespace TurboTyper
         public MainPage()
         {
             InitializeComponent();
+            DisplayScrollViewer.SizeChanged += DisplayScrollViewer_SizeChanged;
+        }
+
+        private void DisplayScrollViewer_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            double scale = Math.Min(e.NewSize.Width / 800, e.NewSize.Height / 600);
+            DisplayGrid.RenderTransform = new ScaleTransform
+            {
+                ScaleX = scale * 1.8, 
+                ScaleY = scale * 1.8
+            };
         }
     }
 }
