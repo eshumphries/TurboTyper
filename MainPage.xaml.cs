f8947428f32963a2e293bbcde3cb813bd2f1acc7
eshumphries <eshumphries@outlook.com>
2025-06-18 15:23:04 -0400

    Made code comments that show the sentences to be typed in the game and added help text when the Help button is clicked

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -26,7 +26,7 @@ namespace TurboTyper
 
         private void SButton_Click(object sender, RoutedEventArgs e)
         {
-            
+            DisplayText.Text = "Just type the sentences that appear on the\nscreen in the textbox.  Type fast, because\nthere's a time limit.  Type the sentences on\ntime to go to the next level.  The next level\nwill increase in difficulty.  Finish all 6 levels to\nwin the game.";
         }
 
         private void DButton_Click(object sender, RoutedEventArgs e)
@@ -43,5 +43,13 @@ namespace TurboTyper
                 ScaleY = scale * 1.8
             };
         }
+
+        // Here's the sentences to type in the game:
+        // 1. Just type this.
+        // 2. How fast can you type?
+        // 3. Excellent job so far, but it gets trickier!
+        // 4. You've passed Level 1, 2, and 3. Very good.
+        // 5. The 6th and final level's coming up! Are you sure you're ready?
+        // 6. You asked for it! This level's difficulty is crazier, longer, and harder than the others. Is it beatable?
     }
 }
